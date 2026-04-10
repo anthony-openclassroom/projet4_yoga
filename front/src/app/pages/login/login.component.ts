10af62cbc871c9f9a86c4dbce8858909138a4c71
Anthony Gorski <anthony.developpeurweb@gmail.com>
2026-04-10 13:46:35 +0200

    refactor(login.component.ts): simplify form initialization and improve code readability by using concise syntax style(login.component.ts): format code for consistency and adhere to style guidelines

--- front/src/app/pages/login/login.component.ts
+++ front/src/app/pages/login/login.component.ts
@@ -2,18 +2,18 @@ import { Component, DestroyRef, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { SessionInformation } from 'src/app/core/models/sessionInformation.interface';
-import { SessionService } from 'src/app/core/service/session.service';
+import { SessionInformation } from '../../core/models/sessionInformation.interface';
+import { SessionService } from '../../core/service/session.service';
 import { LoginRequest } from '../../core/models/loginRequest.interface';
 import { AuthService } from '../../core/service/auth.service';
-import {MaterialModule} from "../../shared/material.module";
+import { MaterialModule } from '../../shared/material.module';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   imports: [CommonModule, MaterialModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
   private authService = inject(AuthService);
@@ -26,32 +26,21 @@ export class LoginComponent {
   public onError = false;
 
   public form = this.fb.group({
-    email: [
-      '',
-      [
-        Validators.required,
-        Validators.email
-      ]
-    ],
-    password: [
-      '',
-      [
-        Validators.required,
-        Validators.min(3)
-      ]
-    ]
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.min(3)]],
   });
 
   public submit(): void {
     const loginRequest = this.form.value as LoginRequest;
-    this.authService.login(loginRequest)
+    this.authService
+      .login(loginRequest)
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe({
         next: (response: SessionInformation) => {
           this.sessionService.logIn(response);
           this.router.navigate(['/sessions']);
         },
-        error: error => this.onError = true,
+        error: (error) => (this.onError = true),
       });
   }
 }
